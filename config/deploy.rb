c597f652d73944fa491f50ce8098ddeda220d6dd
francesco.loreti <francesco.loreti@isprambiente.it>
2022-02-26 15:30:59 +0100

    Add chmod

--- config/deploy.rb
+++ config/deploy.rb
@@ -18,6 +18,9 @@ namespace :deploy do
   task :yarn_install do
     on roles(:web) do
       within release_path do
+        execute("cd #{release_path} && chmod 755 bin/yarn")
+        execute("cd #{release_path} && chmod 755 bin/rails")
+        execute("cd #{release_path} && chmod 755 bin/bundle")
         execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
       end
     end
